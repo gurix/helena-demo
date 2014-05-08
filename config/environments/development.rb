58c1a87b959e8a40a2e07d93046f175dab953030
Markus Graf <info@markusgraf.ch>
2014-05-08 17:17:17 +0200

    configure mailing and log to file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,4 +31,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :file
+  config.action_mailer.file_settings = { location: 'log/mails' }
 end
