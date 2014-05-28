2ce0d82eecbc359b44e6116d348158f5b355ba35
Markus Graf <info@markusgraf.ch>
2014-05-28 12:18:56 +0200

    fixes for running on heroku

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module HelenaDemo
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.i18n.enforce_available_locales = false
+
     config.generators do |g|
       g.template_engine :haml # We are using Haml by default
       g.test_framework :rspec # We use rspec instead of unit tests
