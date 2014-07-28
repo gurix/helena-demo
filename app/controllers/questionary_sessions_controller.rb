d75524244d95999558039a8805e34e75412c2d22
Markus Graf <info@markusgraf.ch>
2014-07-28 11:07:31 +0200

    * german template with ember.js * fixed pdf_url * using session_report variable

--- app/controllers/questionary_sessions_controller.rb
+++ app/controllers/questionary_sessions_controller.rb
@@ -2,8 +2,8 @@ class QuestionarySessionsController < Helena::SessionsController
   def show
     @session = QuestionarySession.find_by view_token: params[:token]
     @version = @session.survey.versions.find(@session.version_id)
-    content = Haml::Engine.new(File.read(Rails.root.join "db/seeds/files/report_satisfaction_scale_survey.en.html.haml")).render
-    #content = @version.session_report
+    #content = Haml::Engine.new(File.read(Rails.root.join "db/seeds/files/report_satisfaction_scale_survey.en.html.haml")).render
+    content = @version.session_report.html_safe
     respond_to do |format|
       format.html { render html: content, layout: true }
       format.json { render json: @session }
