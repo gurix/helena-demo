18a7534045b3b090b95af95b0f717f682ea243e0
Markus Graf <info@markusgraf.ch>
2014-08-04 14:47:04 +0200

    remove more unnecessary code

--- app/controllers/questionary_sessions_controller.rb
+++ app/controllers/questionary_sessions_controller.rb
@@ -1,14 +1,13 @@
 class QuestionarySessionsController < Helena::SessionsController
-  def show
-    @session = QuestionarySession.find_by view_token: params[:token]
-    @version = @session.survey.versions.find(@session.version_id)
-    # content = Haml::Engine.new(File.read(Rails.root.join "db/seeds/files/report_satisfaction_scale_survey.en.html.haml")).render
-    content = @version.session_report.html_safe
-    respond_to do |format|
-      format.html { render html: content, layout: true }
-      format.json { render json: @session }
-    end
-  end
+  # def show
+  #  @session = QuestionarySession.find_by view_token: params[:token]
+  #  @version = @session.survey.versions.find(@session.version_id)
+  #  content = Haml::Engine.new(File.read(Rails.root.join "db/seeds/files/report_satisfaction_scale_survey.en.html.haml")).render
+  #  respond_to do |format|
+  #    format.html { render html: content, layout: true }
+  #    format.json { render json: @session }
+  #  end
+  # end
 
   def new
     survey = Helena::Survey.find params[:survey_id]
