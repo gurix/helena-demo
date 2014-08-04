13119fc6c088ea734804d16a5b86ffcbc6a10dee
Markus Graf <info@markusgraf.ch>
2014-08-04 15:57:25 +0200

    added more tests for the content

--- app/controllers/questionary_sessions_controller.rb
+++ app/controllers/questionary_sessions_controller.rb
@@ -3,7 +3,7 @@ class QuestionarySessionsController < Helena::SessionsController
     @session = QuestionarySession.find_by view_token: params[:token]
     @version = @session.survey.versions.find(@session.version_id)
     # content = Haml::Engine.new(File.read(Rails.root.join "db/seeds/files/report_satisfaction_scale_survey.en.html.haml")).render
-    content = @version.session_report.html_safe
+    content = @version.session_report.html_safe if @version.session_report.present?
     respond_to do |format|
       format.html { render html: content, layout: true }
       format.json { render json: @session }
@@ -13,6 +13,6 @@ class QuestionarySessionsController < Helena::SessionsController
   def new
     survey = Helena::Survey.find params[:survey_id]
     session = QuestionarySession.new survey: survey, version: survey.newest_version, user: current_user
-    session.save and redirect_to helena.edit_session_path(session.token), status: :found
+    session.save && redirect_to(helena.edit_session_path(session.token), status: :found)
   end
 end
--- spec/controllers/questionary_sessions_controller_spec.rb
+++ spec/controllers/questionary_sessions_controller_spec.rb
@@ -1,20 +1,41 @@
 require 'spec_helper'
 
 describe QuestionarySessionsController do
-  before do
-    @user = create :user
-    @user.confirm!
-    @survey = create :survey
-    @baseversion = @survey.versions.create version: 0
-  end
+  let(:survey) { create :survey }
+  let(:baseversion) { survey.versions.create version: 0 }
 
   it 'generates a new session' do
-    sign_in @user
-
-    published_version = Helena::VersionPublisher.publish @baseversion
+    published_version = Helena::VersionPublisher.publish baseversion
     published_version.notes = 'bla bla'
     published_version.save
 
-    expect { get :new, survey_id: @survey }.to change { QuestionarySession.count }.by(1)
+    expect { get :new, survey_id: survey }.to change { QuestionarySession.count }.by(1)
+  end
+
+  it 'returns a json result of the current session' do
+    version = Helena::VersionPublisher.publish baseversion
+    version.save
+    session = QuestionarySession.create survey: survey, version: version, answers: [
+      build(:string_answer, code: 'string_answer_1', value: 'abc'),
+      build(:integer_answer, code: 'integer_answer_1', value: '123')
+    ]
+
+    get :show, token: session.view_token, format: :json
+
+    result = ActiveSupport::JSON.decode(response.body)
+    expect(result['answer']['string_answer_1']).to eq 'abc'
+    expect(result['answer']['integer_answer_1']).to eq 123
+  end
+
+  it 'return an html result of the current session' do
+    version = Helena::VersionPublisher.publish baseversion
+    version.session_report = 'Honeymoonlake'
+    version.save
+
+    session = QuestionarySession.create survey: survey, version: version
+
+    get :show, token: session.view_token
+
+    expect(response.body).to render_template(layout: true)
   end
 end
