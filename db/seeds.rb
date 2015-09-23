d0a0edcb8024d88c9e215fbad3ada2b0bc943b3d
Markus Graf <info@markusgraf.ch>
2015-09-23 14:35:24 +0200

    fixed broken seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'factory_girl'
-require 'ffaker'
+require 'faker'
 
 include FactoryGirl::Syntax::Methods
 
