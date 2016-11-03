a3658e42a19e8b55a82f6744a0f1dfe27b167f4d
Markus Graf <info@markusgraf.ch>
2016-11-03 10:54:03 +0100

    Update code to the newest version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
   def set_locale
--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,7 +1,11 @@
 class OmniauthController < ApplicationController
   def localized
-    # Just save the current locale in the session and redirect to the unscoped path as before
     session[:omniauth_login_locale] = I18n.locale
-    redirect_to user_omniauth_authorize_path(params[:provider])
+    case params[:provider]
+    when 'twitter'
+      redirect_to user_twitter_omniauth_authorize_path
+    when 'facebook'
+      redirect_to user_facebook_omniauth_authorize_path
+    end
   end
 end
